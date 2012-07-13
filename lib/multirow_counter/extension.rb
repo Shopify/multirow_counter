99d1cc3dcd20798be9e5594df4df97d6946dbfd0
Jesse Storimer <jstorimer@gmail.com>
2012-07-13 14:55:00 -0700

    Refactored Extension

--- lib/multirow_counter/extension.rb
+++ lib/multirow_counter/extension.rb
@@ -1,22 +1,25 @@
 module MultirowCounter
   module Extension
     def multirow_counter(counter_name, options)
-      class_name = self.name
       num_rows = options[:rows] || raise(ArgumentError, "You need to specify how many rows will be used eg. :rows => 3")
+      class_name = self.name
 
       creator = CounterModelCreator.new(counter_name.to_s, class_name)
       const = creator.create
 
       # define getter method
       define_method(counter_name) do
-        const.where(class_name.foreign_key => id).sum(:value)
+        counter_relation = const.where(class_name.foreign_key => id)
+        counter_relation.sum(:value)
       end
 
       # define increment method
       define_method("increment_#{counter_name}") do
-        const.where(class_name.foreign_key => id).where(:counter_id => rand(3)+1).limit(1).update_all("value = value+1")
-      end
+        counter_relation = const.where(class_name.foreign_key => id)
+        randomly_selected_counter_row = rand(num_rows) + 1
 
+        counter_relation.where(:counter_id => randomly_selected_counter_row).limit(1).update_all("value = value+1")
+      end
     end
   end
 end

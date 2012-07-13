046793c916a3574c497a90b5f294cfcdfe8dec49
Jesse Storimer <jstorimer@gmail.com>
2012-07-13 14:50:11 -0700

    Extract Extension

--- lib/multirow_counter.rb
+++ lib/multirow_counter.rb
@@ -1,31 +1,7 @@
+require 'multirow_counter/extension'
 require 'multirow_counter/counter_model_creator'
 
 module MultirowCounter
-  module Extension
-    def multirow_counter(counter_name, options)
-      class_name = self.name
-      num_rows = options[:rows] || raise(ArgumentError, "You need to specify how many rows will be used eg. :rows => 3")
-
-      creator = CounterModelCreator.new(counter_name.to_s, class_name)
-      const = creator.create
-
-      # define getter method
-      define_method(counter_name) do
-        const.where(class_name.foreign_key => id).sum(:value)
-      end
-
-      # define increment method
-#      define_method("increment_#{counter_name}") do
-#        const.where(class_name.foreign_key => id).where("counter_id = FLOOR(RAND() * #{num_rows}) + 1").limit(1).update_all("value = value+1")
-#      end
-
-      define_method("increment_#{counter_name}") do
-        const.where(class_name.foreign_key => id).where(:counter_id => rand(3)+1).limit(1).update_all("value = value+1")
-      end
-
-    end
-  end
-
   if defined?(::Rails)
     class Railtie < Rails::Railtie
       initializer 'multirow-counter extension' do
